303bffad6e92e226573a8d4f846807528f61b70e
Mario Vilas <mvilas@gmail.com>
2013-07-27 13:30:44 +0200

    Fixed newlines and tabs.

--- lib/hostchecker.py
+++ lib/hostchecker.py
@@ -8,16 +8,15 @@ import sys
 import socket
 
 class Checker():
-	def __init__(self, hosts):
-		self.hosts = hosts
-		self.realhosts=[]
+    def __init__(self, hosts):
+        self.hosts = hosts
+        self.realhosts=[]
 
-	def check(self):
-	 	for x in self.hosts:
-			try:
-				res=socket.gethostbyname(x)
-				self.realhosts.append(res+":"+x)	
-			except Exception, e:
-				pass
-		return self.realhosts
-		
+    def check(self):
+        for x in self.hosts:
+            try:
+                res=socket.gethostbyname(x)
+                self.realhosts.append(res+":"+x)
+            except Exception, e:
+                pass
+        return self.realhosts
